94c95fb71457a9ff3551986bdb0d14cb1121a70d
Ahmed Fahmi <amfahmi.96@gmail.com>
2023-03-07 11:43:58 +0200

    Lab4

---
+++ Lab4/server.js
@@ -0,0 +1,18 @@
+const express=require("express")
+const app = express();
+const http = require("http")
+const server=http.createServer(app);
+const { Server }=require("socket.io")
+const io=new Server(server)
+
+app.get("/",(req,res)=>res.sendFile(__dirname + "/index.html"))
+
+io.on("connection",(socket)=>{console.log("user connected");
+socket.on("disconnect",()=>{console.log("user disconnected")});
+socket.on("chat message",(msg)=>{io.emit("chat message",msg)})
+});
+
+
+
+
+server.listen(9000,()=>console.log("listening on port 9000"))
